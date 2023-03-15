423b3688e4696892eec119266feec95290e2b3e0
JulienParis26 <71380678+JulienParis26@users.noreply.github.com>
2023-03-15 09:20:51 +0100

    Modify home page

--- src/main/webapp/app/entities/fabric/fabric.module.ts
+++ src/main/webapp/app/entities/fabric/fabric.module.ts
@@ -5,9 +5,12 @@ import { FabricDetailComponent } from './detail/fabric-detail.component';
 import { FabricUpdateComponent } from './update/fabric-update.component';
 import { FabricDeleteDialogComponent } from './delete/fabric-delete-dialog.component';
 import { FabricRoutingModule } from './route/fabric-routing.module';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [SharedModule, FabricRoutingModule],
+  imports: [SharedModule, FabricRoutingModule, MatInputModule, MatButtonModule, MatIconModule],
   declarations: [FabricComponent, FabricDetailComponent, FabricUpdateComponent, FabricDeleteDialogComponent],
 })
 export class FabricModule {}
